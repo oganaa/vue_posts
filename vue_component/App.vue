5f9cf31f6d8a435c8d15ea37963c8159d4c86408
Starsoft pc1 <oganaa2472@gmail.com>
2020-12-21 13:02:06 +0800

    VUE TEST 2

---
+++ vue_component/App.vue
@@ -0,0 +1,57 @@
+<template>
+   <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent='Quote'">
+                    Хашилт
+                </button>
+                <button @click="selectedComponent='Author'">
+                    Зохиогч
+                </button>
+                <button @click="selectedComponent='New'">
+                    Шинэ
+                </button>
+
+                {{selectedComponent}}
+                <hr>
+                <keep-alive> 
+
+                <component :is="selectedComponent">
+                </component>
+                </keep-alive>
+                <!-- <quote @click="selectedComponent='New'">
+                    <h1 slot = "title">
+                        {{qTitle}}
+                    </h1>
+                    <small >Hello таслал</small>
+
+                </quote> -->
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import Quote from './components/Quote';
+    import Author from './components/Author';
+    import New from './components/New';
+    export default {
+        data(){
+            return {
+                selectedComponent:'Quote',
+                qTitle:'Гарчиг таслөл.....'
+            }
+        },
+        components: {   
+            Quote,
+            Author,
+            New
+        }
+    }
+</script>
+
+<style>
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
+</style>
---
+++ vue_component/components/New.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>New Compoment</h1>
+    </div>
+</template>
+<script>
+export default {
+    created(){
+          console.log('created uuslee')
+    },
+    mounted(){
+          console.log('mounted uuslee')
+    },
+    destroyed(){
+        console.log('ustlaa')
+    },
+    deactivated(){
+         console.log('idwehgui bolloo')
+    },
+    activated(){
+         console.log('idwehtei bolloo')
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
